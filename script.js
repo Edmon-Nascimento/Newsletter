f589bb835fdb9231a069283dfcdf43a8d7feb927
EDMON NASCIMENTO SANTOS <eddie@60s1pt.onmicrosoft.com>
2025-02-20 12:36:06 -0300

    style finalization
    had some troubles with the alert display
    index.html modifications

---
+++ script.js
@@ -0,0 +1,7 @@
+function showalert() {
+    document.querySelector("section.alert").classList.add("show");
+}
+
+function hidealert() {
+    document.querySelector("section.alert").classList.remove("show");
+}
